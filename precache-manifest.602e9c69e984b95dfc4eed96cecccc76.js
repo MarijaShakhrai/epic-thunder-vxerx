3e4bdf090d41a0ba2f652d621ef70d9dc1c00a9d
Maria Shakhrai <marija.shahrai@gmail.com>
2020-04-12 16:20:26 +0300

    Updates

---
+++ precache-manifest.602e9c69e984b95dfc4eed96cecccc76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424f487a3461a3765b820e1929374ac5",
+    "url": "/epic-thunder-vxerx/index.html"
+  },
+  {
+    "revision": "915128cc57b1b979a5c2",
+    "url": "/epic-thunder-vxerx/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "63eac96dd2aa005583e0",
+    "url": "/epic-thunder-vxerx/static/css/main.f3736ff6.chunk.css"
+  },
+  {
+    "revision": "915128cc57b1b979a5c2",
+    "url": "/epic-thunder-vxerx/static/js/2.6b4cc6f1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/epic-thunder-vxerx/static/js/2.6b4cc6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63eac96dd2aa005583e0",
+    "url": "/epic-thunder-vxerx/static/js/main.7f7a6ff4.chunk.js"
+  },
+  {
+    "revision": "fe4a298702374d85a533",
+    "url": "/epic-thunder-vxerx/static/js/runtime-main.926aa7b2.js"
+  }
+]);
